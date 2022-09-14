850957ea2a0ca7d81243d746571cad8c18215332
ayyubieied <ayyubieied@gmail.com>
2022-09-14 09:07:15 +0700

    Tugas 2 Selesai

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
+
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_catalog': data_barang_katalog,
+    'nama': 'Ied Mubaraque Sultan Salahuddine El Ayyubie',
+    'id': '2106751120',
+    }
+    return render(request, "katalog.html", context)
+    
